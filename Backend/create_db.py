9c96bb8167fc1b9f9bc5f8e27773922502dfdd22
kaiwxn <kwyang0620@icloud.com>
2025-04-11 18:12:43 +0200

    deleted unnecessary content

--- Backend/create_db.py
+++ Backend/create_db.py
@@ -1,15 +1,6 @@
 
 import sqlite3
 
-try:
-    with sqlite3.connect("Backend/project.db") as conn:
-        print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
-
-except sqlite3.OperationalError as e:
-    print("Failed to open database:", e)
-
-import sqlite3
-
 connection = sqlite3.connect('Backend/project.db')
 
 with open('Backend/init_db.sql') as f:
