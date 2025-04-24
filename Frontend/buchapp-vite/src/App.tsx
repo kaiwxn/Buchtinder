705695e5f1f439596e85e5772b9bfa5115e92bd3
kaiwxn <kwyang0620@icloud.com>
2025-04-24 17:29:21 +0200

    added login token + save in sessionstorage

--- Frontend/buchapp-vite/src/App.tsx
+++ Frontend/buchapp-vite/src/App.tsx
@@ -1,29 +1,35 @@
 import { Routes, Route } from 'react-router'
+import { useEffect, useState } from 'react'
 
 import Navbar from './components/Navbar'
-
-import './App.css'
 import Home from './pages/Home'
 import Friends from './pages/Friends'
 import Books from './pages/Books'
 import Footer from './components/Footer'
 import Login from './pages/Login/Login'
 
+import './App.css'
+
 function App() {
+  const [token, setToken] = useState(() => sessionStorage.getItem("token") || "");
 
-  return (
+  useEffect(() => {
+    token ? sessionStorage.setItem("token", token) : sessionStorage.removeItem("token");
+  }, [token]);
+
+  return token ? (
     <>
-      <Login/>
-
-      {/* <Navbar/>
-        <Routes>        
-          <Route path="/"  element={<Home/>} />
-          <Route path="/freunde"  element={<Friends />}/>
-          <Route path="/buecher"  element={<Books/>}/>
-        </Routes>
-      <Footer/> */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/freunde" element={<Friends />} />
+        <Route path="/buecher" element={<Books />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  ) : (
+    <Login setToken={setToken} />
+  );
 }
 
-export default App
+export default App;
--- Frontend/buchapp-vite/src/pages/Login/Login.tsx
+++ Frontend/buchapp-vite/src/pages/Login/Login.tsx
@@ -2,7 +2,17 @@ import { BookCheck } from "lucide-react";
 import PasswordInput from "./PasswordInput";
 import UsernameInput from "./UsernameInput";
 
-function Login() {
+type LoginProps = {
+	setToken: (token: string) => void;
+};
+
+function Login({ setToken }: LoginProps) { 
+
+	async function handleLogin() {
+		const token = "yourUserToken";
+		setToken(token); // This updates App state and sessionStorage
+	};
+
 	return (
 		<>
 			<div className="mt-3 mx-4 mb-5">
@@ -12,22 +22,30 @@ function Login() {
 			</div>
 			<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
 				<div className="sm:mx-auto sm:w-full sm:max-w-96">
-					<h1 className="mt-10 text-[33px]/8 w-full">Dein nächstes Buch wartet schon.</h1>
+					<h1 className="mt-10 text-[33px]/8 w-full">
+						Dein nächstes Buch wartet schon.
+					</h1>
 					<p className="mt-3 text-2xl text-gray-600">Melde dich zuerst an.</p>
 				</div>
 
 				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-					
-					<UsernameInput/>
-					
-					<PasswordInput/>
-					
+					<UsernameInput />
+
+					<PasswordInput />
+
 					<div className="flex mt-6 w-full justify-between">
-						<button className="btn w-45">Login</button>
-						<button className="btn w-45 bg-blue-500 text-white">Registrieren</button>
+						<button className="btn w-45" onClick={handleLogin}>
+							Login
+						</button>
+						<button className="btn w-45 bg-blue-500 text-white">
+							Registrieren
+						</button>
 					</div>
 					<div className="flex mt-6 justify-center text-center mx-20">
-						<p className="text-xs">Indem du fortfährst, erklärst du, dass du <span className="underline">diesen Bedingungen</span> zustimmst.</p>
+						<p className="text-xs">
+							Indem du fortfährst, erklärst du, dass du{" "}
+							<span className="underline">diesen Bedingungen</span> zustimmst.
+						</p>
 					</div>
 				</div>
 			</div>
