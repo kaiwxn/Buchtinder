18e1664920f5ceae18faff4d5b616b10b15cbdd8
kaiwxn <kwyang0620@icloud.com>
2025-05-13 13:40:39 +0200

    working search

--- Frontend/src/components/BookSearchModal.tsx
+++ Frontend/src/components/BookSearchModal.tsx
@@ -1,19 +1,73 @@
 import { Plus, Search, X } from "lucide-react";
+import { useEffect, useState } from "react";
 
 type BookSearchModalProps = {
     onClose: () => void;
 };
 
-function BookSearchModal({ onClose }: BookSearchModalProps) {
+type BookJsonObject = {
+    volume_id: string;
+    title: string;
+    authors: string[];
+    thumbnail?: string;
+    isbn?: string;
+    info_link: string;
+    categories?: string[];
+    language?: string;
+};
 
-    
+function BookSearchModal({ onClose }: BookSearchModalProps) {
+    const [query, setQuery] = useState("");
+    const [results, setResults] = useState<BookJsonObject[]>([]);
+    const [page, setPage] = useState(0);
+    const [loading, setLoading] = useState(false);
+
+    async function searchBooks(query: string, page: number = 0) {
+        const response = await fetch(
+            `http://127.0.0.1:5000/books/search_books?q=${encodeURIComponent(query)}&page=${page}`,
+        );
+        if (!response.ok) {
+            throw new Error("Failed to fetch books");
+        }
+        return await response.json();
+    }
+
+    const fetchBooks = async () => {
+        setLoading(true);
+        try {
+            const books = await searchBooks(query, page);
+            setResults(books);
+        } catch (error) {
+            console.error("Error fetching books:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleNextPage = () => {
+        setPage(page + 1);
+    };
+
+    const handlePreviousPage = () => {
+        if (page > 0) {
+            setPage(page - 1);
+        }
+    };
+
+    useEffect(() => {
+        fetchBooks();
+        document
+            .getElementById("book-results-list")
+            ?.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to top
+    }, [page]);
+
     return (
         <div
             className="fixed inset-0 z-50 flex items-center justify-center"
             onClick={onClose}
         >
             <div
-                className="w-11/12 rounded-xl bg-white p-5 pt-8 shadow-lg lg:w-1/2"
+                className="h-[85vh] w-11/12 rounded-xl bg-white p-5 pt-8 shadow-lg lg:w-1/2"
                 onClick={(e) => e.stopPropagation()}
             >
                 <div className="mb-4 flex items-center justify-between">
@@ -34,45 +88,86 @@ function BookSearchModal({ onClose }: BookSearchModalProps) {
                         <input
                             type="search"
                             required
-                            placeholder="Suche"
+                            placeholder="Suchbegriff eingeben..."
                             className="join-item ml-2"
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                            onKeyDown={(e) => {
+                                if (e.key === "Enter") {
+                                    fetchBooks();
+                                }
+                            }}
                         />
                     </label>
-                    <select className="select join-item w-1/5">
+
+                    <select className="select join-item w-fit">
                         <option disabled selected>
                             Filter
                         </option>
                         <option>Neuste</option>
                         <option>Relevanteste</option>
                     </select>
+                    <button
+                        className="btn btn-neutral join-item shadow-none"
+                        onClick={() => {
+                            setPage(0);
+                            fetchBooks();
+                        }}
+                    >
+                        Suchen
+                    </button>
                 </div>
-
-                <div className="max-h-[60vh] space-y-2 overflow-x-clip overflow-y-auto">
-                    {Array.from({ length: 10 }).map((_, index) => (
+                {loading && (
+                    <div className="flex items-center justify-center">
+                        <span className="loading loading-spinner loading-xl"></span>
+                    </div>
+                )}
+                <div
+                    id="book-results-list"
+                    className="max-h-[60vh] space-y-2 overflow-x-clip overflow-y-auto"
+                >
+                    {results.map((book, index) => (
                         <div key={index}>
                             <div className="flex h-30">
                                 <img
-                                    src="https://m.media-amazon.com/images/I/419JCm3Tr+L._SY445_SX342_PQ57_.jpg"
+                                    src={
+                                        book.thumbnail ||
+                                        "https://thumbs.dreamstime.com/b/transparent-seamless-pattern-background-checkered-simulation-alpha-channel-png-wallpaper-empty-gird-grid-vector-illustration-308566526.jpg"
+                                    }
                                     alt="Cover"
                                     width={80}
                                 />
                                 <div className="flex p-5 pb-0">
                                     <div className="">
                                         <a
-                                            href="https://www.google.de/books/edition/Die_Tribute_von_Panem_X_Das_Lied_von_Vog/u-HkDwAAQBAJ?hl=de&gbpv=0" // Link zu Google Books
+                                            href={book.info_link}
                                             className="mb-1 line-clamp-1 text-xl font-medium overflow-ellipsis hover:underline"
                                         >
-                                            Tribute von Panem
+                                            {book.title}
                                         </a>
-                                        <p className="mb-2"> Susanne Collins</p>
+                                        <p className="mb-2 sm:max-w-[300px] max-w-[150px] overflow-hidden text-ellipsis whitespace-nowrap">
+                                            {book.authors
+                                                ?.slice(0, 3)
+                                                .join(
+                                                    ", ",
+                                                ) // First 3 authors
+                                            }
+                                        </p>
 
                                         <div className="justify-between space-x-2">
-                                            <div className="badge badge-soft p-3">
-                                                Fantasy
-                                            </div>
-                                            <div className="badge badge-soft p-3">
-                                                Krimi
-                                            </div>
+                                            {book.categories?.map(
+                                                (
+                                                    category: string,
+                                                    index: number,
+                                                ) => (
+                                                    <div
+                                                        key={index}
+                                                        className="badge badge-soft max-w-[300px] overflow-hidden text-ellipsis whitespace-nowrap"
+                                                    >
+                                                        {category}
+                                                    </div>
+                                                ),
+                                            )}
                                         </div>
                                     </div>
                                 </div>
@@ -89,10 +184,24 @@ function BookSearchModal({ onClose }: BookSearchModalProps) {
                             <div className="divider"></div>
                         </div>
                     ))}
-                    <div className="flex join justify-center">
-                        <button className="join-item btn btn-ghost">«</button>
-                        <button className="join-item btn btn-ghost">1</button>
-                        <button className="join-item btn btn-ghost">»</button>
+                    <div className="join flex justify-center">
+                        <button
+                            className="join-item btn btn-ghost"
+                            onClick={handlePreviousPage}
+                            disabled={page === 0}
+                        >
+                            «
+                        </button>
+                        <button className="join-item btn btn-ghost">
+                            {page}
+                        </button>
+                        <button
+                            className="join-item btn btn-ghost"
+                            onClick={handleNextPage}
+                            disabled={results.length === 0}
+                        >
+                            »
+                        </button>
                     </div>
                 </div>
             </div>
