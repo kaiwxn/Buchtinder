58965043efa18e146c1d36a3d13bd809fa810444
kaiwxn <kwyang0620@icloud.com>
2025-06-02 16:28:27 +0200

    refactoring: Split into cardstack

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
-import Card from "./TinderCard";
+import CardStack from "./TinderCardStack";
+
+
 
 const cardsData = Array.from({ length: 10 }, (_, i) => ({
     id: i,
@@ -7,28 +8,10 @@ const cardsData = Array.from({ length: 10 }, (_, i) => ({
 }));
 
 
-
-function CardStack() {
-    const [cards, setCards] = useState(cardsData);
-
-    return (
-        <div className="mt-5 flex justify-center">
-            {cards.map((card: any) => (
-                <Card
-                    key={card.id}
-                    {...card}
-                    setCards={setCards}
-                    cards={cards}
-                />
-            ))}
-        </div>
-    );
-}
-
 function Home() {
     return (
         <div className="mx-0.5 mt-10 min-h-screen justify-center px-4 sm:mx-60 sm:px-10 lg:px-60">
-            <CardStack />
+            <CardStack data={cardsData} />
         </div>
     );
 }
