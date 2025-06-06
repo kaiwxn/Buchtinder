a8ed8c03ef025a1dfe6b4468fcc7e841a724e867
Lukas <150171669+DieKiwii@users.noreply.github.com>
2025-06-06 20:58:26 +0200

    completing tinder backend

--- Backend/services/tinder_service.py
+++ Backend/services/tinder_service.py
@@ -7,6 +7,8 @@ from services.tinder_algorithm import find_similar_users
 def get_card_info(user_id:int):
     user_ids = find_similar_users(user_id)
     results = []
+    print("Input user_id:", user_id)
+    print("Similar users returned:", user_ids)
 
     for user_id in user_ids:
         user = db.session.query(Users).filter_by(id=user_id).first()
@@ -17,10 +19,13 @@ def get_card_info(user_id:int):
         added_books_from_user = db.session.query(UserToBooks).filter_by(user_id=user_id).all()
 
         categories = []
+        thumbnails = []
 
         for book in added_books_from_user:
             volume_id = book.volume_id
             book_info, status = fetch_book_info(volume_id)
+            thumbnails_list = book_info.get('thumbnail', [])
+            thumbnails.append(thumbnails_list)
 
             if status != 200:
                 continue
@@ -31,8 +36,11 @@ def get_card_info(user_id:int):
         category_counter = Counter(categories)
         most_common_categories = [category for category, count in category_counter.most_common(2)]
         results.append({
+            'user_id': user_id,
             'username': username,
-            'top_categories': most_common_categories
+            'top_categories': most_common_categories,
+            'profileImage': 'https://as1.ftcdn.net/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg',
+            'thumbnails': thumbnails
             })
     
     return results, 200
