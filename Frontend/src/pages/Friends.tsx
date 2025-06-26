23f2af0f518cc8cb123b6122dd3de71d4f5e232c
kaiwxn <kwyang0620@icloud.com>
2025-06-26 12:19:47 +0200

    bessere  freundesliste

--- Frontend/src/pages/Friends.tsx
+++ Frontend/src/pages/Friends.tsx
@@ -7,6 +7,9 @@ type Friend = {
     bookCount: number;
 };
 
+const BACKUP_PROFILE_IMAGE_SRC =
+    "https://as1.ftcdn.net/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg";
+
 function Friends() {
     const [friends, setFriends] = useState<Friend[]>([]);
 
@@ -32,15 +35,36 @@ function Friends() {
     return (
         <div className="mt-10 min-h-screen justify-center px-4 sm:px-10 lg:px-60">
             <h1 className="text-4xl">Freunde</h1>
-            <p>Das sind deine Freunde:</p>
-            <ul className="mt-6">
-                {friends.map((friend) => (
-                    <li key={friend.id} className="border-b py-2">
-                        Name: {friend.name}
-                    </li>
-                ))}
-            </ul>
+            
+            {friends.map((friend, index) => (
+                <div key={index}>
+                    <div className="mt-5 flex h-24">
+                        <div className="avatar flex items-center justify-center">
+                            <div className="h-15 w-15 rounded-full">
+                                <img
+                                    src={BACKUP_PROFILE_IMAGE_SRC}
+                                    alt="Cover"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="flex p-5 pb-0">
+                            <div>
+                                <p className="mb-1 line-clamp-1 text-xl font-medium overflow-ellipsis hover:underline">
+                                    {friend.name}
+                                </p>
+                                <p className="text-sm text-gray-500">
+                                    {friend.bookCount || "Keine"} Bücher,
+                                    am {friend.createdAt ? friend.createdAt.toLocaleDateString("de-DE") : "unbekannten Zeitpunkt"} beigetreten.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="divider"/>
+                </div>
+            ))}
         </div>
+
     );
 }
 
